b5a5de7528ef61076ec7313ec1ac9811733ca7fc
Adl1coder <adilegunngorr@gmail.com>
2023-09-09 17:38:14 +0300

    güncelleme

--- app/src/main/java/com/example/notlar/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/notlar/fragments/SignUpFragment.kt
@@ -14,58 +14,71 @@ import com.example.notlar.databinding.FragmentSignUpBinding
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 
-
-
-
 class SignUpFragment : Fragment() {
 
-private lateinit var auth: FirebaseAuth
-private  lateinit var navControl: NavController
-private lateinit var binding: FragmentSignUpBinding
+    private lateinit var navController: NavController
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding= FragmentSignUpBinding.inflate(inflater,container,false)
-
-        // Inflate the layout for this fragment
+        // Fragment için görünümünün oluşturulması
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Gerekli bileşenlerin başlatılması
         init(view)
-        registerEvents()
-    }
-    private fun init(view: View){
-        navControl=Navigation.findNavController(view)
-        auth=FirebaseAuth.getInstance()
-    }
-    private fun registerEvents(){
-        binding.
 
-        binding.nextBtn.setOnClickListener{
-            val email=binding.emailEt.text.toString()
-            val pass=binding.passEt.text.toString()
-            val verifyPass=binding.verifyPassEt.text.toString().trim()
+        // "Giriş Yap" metni tıklandığında, ilgili fragment'a git
+        binding.textViewSignIn.setOnClickListener {
+            navController.navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
 
-if(email.isNotEmpty()&&pass.isNotEmpty()&&verifyPass.isNotEmpty()){
-    if(pass==verifyPass){
-        auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(
-            OnCompleteListener {
-                if(it.isSuccessful){
-                    Toast.makeText(context,"Kayıt Başarılı", Toast.LENGTH_SHORT).show()
-                    navControl.navigate(R.id.action_signUpFragment_to_homeFragment)
-                }else{
-                    Toast.makeText(context,it.exception?.message, Toast.LENGTH_SHORT).show()
+        // "İleri" düğmesine tıklanıldığında kullanıcı kaydını oluştur
+        binding.nextBtn.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passEt.text.toString()
+            val verifyPass = binding.verifyPassEt.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && verifyPass.isNotEmpty()) {
+                if (pass == verifyPass) {
+
+                    // Kullanıcıyı kaydet
+                    registerUser(email, pass)
+
+                } else {
+                    // Şifreler eşleşmiyorsa kullanıcıya uyarı ver
+                    Toast.makeText(context, "Şifreler aynı değil", Toast.LENGTH_SHORT).show()
                 }
-            }
-        )
+            } else
+            // Boş alanlar izin verilmezse kullanıcıya uyarı ver
+                Toast.makeText(context, "Boş alanlar izin verilmez", Toast.LENGTH_SHORT).show()
+        }
+
     }
-}
+
+    private fun registerUser(email: String, pass: String) {
+        // Firebase üzerinde kullanıcı kaydı oluştur
+        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+            if (it.isSuccessful)
+            // Başarılı kayıt durumunda ana ekrana yönlendir
+                navController.navigate(R.id.action_signUpFragment_to_homeFragment)
+            else
+            // Kayıt başarısızsa hata mesajını kullanıcıya göster
+                Toast.makeText(context, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
         }
     }
 
+    private fun init(view: View) {
+        // NavController ve FirebaseAuth'in başlatılması
+        navController = Navigation.findNavController(view)
+        mAuth = FirebaseAuth.getInstance()
+    }
 }
